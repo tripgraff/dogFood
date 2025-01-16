10b213775a46e5af233e0a65b9291efa64fd4017
tripgraff <tripgraffmac@Noutbuk-tripgraff.local>
2025-01-16 21:00:52 +0300

    create first application

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import ReactDOM from 'react-dom';
+import './index.css'
 
 ReactDOM.render(
-	<App/>,
-	document.querySelector("#root")
-);
+	<div className="wrapper">
+		<div className="card">=)</div>
+		<div className="card">OO</div>
+		<div className="card">^^</div>
+		<div className="card">O_o</div>
+	</div>,
+	document.querySelector('#root')
+)
